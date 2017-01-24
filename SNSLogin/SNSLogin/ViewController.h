ca1be8b2df2d2ff03b323b66befd9e3e46fa8859
mmminari <mmminari@smtown.com>
2017-01-24 17:18:05 +0900

    first commit

---
+++ SNSLogin/SNSLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SNSLogin
+//
+//  Created by 김민아 on 2017. 1. 24..
+//  Copyright © 2017년 김민아. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SNSLogin/SNSLoginTests/SNSLoginTests.m
@@ -0,0 +1,39 @@
+//
+//  SNSLoginTests.m
+//  SNSLoginTests
+//
+//  Created by 김민아 on 2017. 1. 24..
+//  Copyright © 2017년 김민아. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SNSLoginTests : XCTestCase
+
+@end
+
+@implementation SNSLoginTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
